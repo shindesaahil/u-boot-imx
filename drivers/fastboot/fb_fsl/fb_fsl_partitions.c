488a7278333d21c598688064e47803f7d5622338
AniketWaJe <aniket.waje@trunexa.com>
2024-08-31 00:11:22 +0530

    fastboot/fb_fsl : fb_fsl_partitions.c :  Added EMMC_BOOT_PART to return address

--- drivers/fastboot/fb_fsl/fb_fsl_partitions.c
+++ drivers/fastboot/fb_fsl/fb_fsl_partitions.c
@@ -69,7 +69,11 @@ static ulong bootloader_mmc_offset(void)
 	} else if (is_imx8mn() || is_imx8mp() || is_imx8dxl() || is_imx8ulp() || is_imx93()) {
 		/* target device is eMMC boot0 partition, bootloader offset is 0x0 */
 		if (env_get_ulong("emmc_dev", 10, 2) == fastboot_devinfo.dev_id)
+		#ifdef CONFIG_USE_EMMC_BOOT_PART
+			return 0x8000;
+		#else
 			return 0;
+		#endif
 		else
 			return 0x8000;
 	}
